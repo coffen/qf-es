b915b9de9ff63a81ab104b2381e4340f3f3b2d34
qiongqi <qiongqi@ixiaopu.com>
2017-11-13 18:19:15 +0800

    添加模板

---
+++ es-core/src/main/java/com/qf/es/model/Template.java
@@ -0,0 +1,23 @@
+package com.qf.es.model;
+
+/**
+ * 
+ * <p>
+ * Project Name: C2C商城
+ * <br>
+ * Description: ES模板
+ * <br>
+ * File Name: Template.java
+ * <br>
+ * Copyright: Copyright (C) 2015 All Rights Reserved.
+ * <br>
+ * Company: 杭州偶尔科技有限公司
+ * <br>
+ * @author 穷奇
+ * @create time：2017年11月13日 下午6:12:44 
+ * @version: v1.0
+ *
+ */
+public class Template {
+
+}
