9b34465a03de66f6f5eabaa17a8274a1d5341129
qiongqi <qiongqi@ixiaopu.com>
2017-12-05 18:24:18 +0800

    修改

---
+++ es-core/src/main/java/com/qf/es/model/Mapping.java
@@ -0,0 +1,58 @@
+package com.qf.es.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 
+ * <p>
+ * Project Name: 淘客
+ * <br>
+ * Description: 映射
+ * <br>
+ * File Name: Mapping.java
+ * <br>
+ * Copyright: Copyright (C) 2015 All Rights Reserved.
+ * <br>
+ * Company: 杭州偶尔科技有限公司
+ * <br>
+ * @author 穷奇
+ * @create time：2017年12月5日 下午5:55:55 
+ * @version: v1.0
+ *
+ */
+public class Mapping {
+	
+	private String typeName;
+	private Map<String, MappingField<?>> fieldMap = new HashMap<String, MappingField<?>>();
+	
+	public Mapping(String typeName) {
+		if (StringUtils.isBlank(typeName)) {
+			throw new RuntimeException("Mapping constructor parameters must not empty.");
+		}
+		this.typeName = typeName;
+	}
+	
+	public String getTypeName() {
+		return typeName;
+	}
+	
+	public boolean addField(MappingField<?> field) {
+		if (field == null || StringUtils.isBlank(field.getPropertyName())) {
+			return false;
+		}
+		String propertyName = field.getPropertyName();
+		boolean contains = fieldMap.containsKey(propertyName);
+		if (!contains) {
+			fieldMap.put(propertyName, field);
+		}
+		return !contains;
+	}
+	
+	public MappingField<?> getField(String propertyName) {
+		return fieldMap.get(propertyName);
+	}
+
+}
--- es-service/src/test/java/com/qf/es/test/MappingTest.java
+++ es-service/src/test/java/com/qf/es/test/MappingTest.java
@@ -1,16 +1,52 @@
 package com.qf.es.test;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import org.elasticsearch.action.ActionFuture;
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingRequestBuilder;
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingResponse;
+import org.elasticsearch.client.IndicesAdminClient;
+import org.elasticsearch.client.transport.TransportClient;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.qf.es.model.MappingField;
+import com.qf.es.model.MappingParameter;
+import com.qf.es.model.field.TextField;
 
 public class MappingTest extends BaseUnit {
 	
 	private final Logger log = LoggerFactory.getLogger(getClass());
 	
+	@Autowired
+	private TransportClient transportClient;
+	
 	@Test
-	public void testMapping() {
+	public void testMapping() throws IOException, InterruptedException, ExecutionException {
+		IndicesAdminClient client = transportClient.admin().indices();		
+		PutMappingRequestBuilder builder = client.preparePutMapping("test");
 		
+		MappingField<TextField> mf = new MappingField<TextField>("testField", new TextField());
+		mf	.addParameter(MappingParameter.STORE.TRUE)
+ 		 	.addParameter(MappingParameter.ANALYZER.IK_SMART)
+ 		 	.addParameter(MappingParameter.INDEX.TRUE)
+ 		 	.addParameter(MappingParameter.BOOST.value(2.0f));
+		
+		Map<String, Object> map = new HashMap<>();
+		Map<String, Object> properties = new HashMap<>();
+		properties.put(mf.getPropertyName(), mf.buildJsonContext());
+		map.put("properties", properties);
+		
+		builder.setType("testType").setSource(map);
+		ActionFuture<PutMappingResponse> future = builder.execute();
+		
+		PutMappingResponse response = future.get();
+		log.error("Response: {}", response.isAcknowledged());
 	}
 	
 }
