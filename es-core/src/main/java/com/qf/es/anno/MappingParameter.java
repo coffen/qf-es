99c52a8ea3c44a8ee6390993eabc6846f25b97dd
qiongqi <qiongqi@ixiaopu.com>
2018-01-22 18:34:39 +0800

    添加注解

---
+++ es-core/src/main/java/com/qf/es/anno/MappingParameter.java
@@ -0,0 +1,36 @@
+package com.qf.es.anno;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 
+ * <p>
+ * Project Name: 淘客
+ * <br>
+ * Description: 映射参数
+ * <br>
+ * File Name: MappingParameter.java
+ * <br>
+ * Copyright: Copyright (C) 2015 All Rights Reserved.
+ * <br>
+ * Company: 杭州偶尔科技有限公司
+ * <br>
+ * @author 穷奇
+ * @create time：2018年1月22日 下午5:18:49 
+ * @version: v1.0
+ *
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MappingParameter {
+	
+	String name();
+	
+	MetaField[] meta();
+
+}
---
+++ es-core/src/main/java/com/qf/es/anno/MetaField.java
@@ -0,0 +1,34 @@
+package com.qf.es.anno;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 
+ * <p>
+ * Project Name: 淘客
+ * <br>
+ * Description: 元数据映射属性
+ * <br>
+ * File Name: MetaField.java
+ * <br>
+ * Copyright: Copyright (C) 2015 All Rights Reserved.
+ * <br>
+ * Company: 杭州偶尔科技有限公司
+ * <br>
+ * @author 穷奇
+ * @create time：2018年1月22日 下午5:20:18 
+ * @version: v1.0
+ *
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MetaField {
+	
+	String name();
+
+}
